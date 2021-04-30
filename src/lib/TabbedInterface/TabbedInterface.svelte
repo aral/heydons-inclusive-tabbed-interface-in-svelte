e2e9de9401bd758abac3bc96b31b190a330c0cf1
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-04-30 17:28:54 +0100

    Clean up old code

--- src/lib/TabbedInterface/TabbedInterface.svelte
+++ src/lib/TabbedInterface/TabbedInterface.svelte
@@ -2,26 +2,18 @@
   import { onMount, setContext } from 'svelte'
   import { writable } from 'svelte/store'
 
-  // export let selectedIndex = 0
-
   const lastTabIndex = writable(-1)
   const lastTabPanelIndex = writable(-1)
-  const activeTabIndex = writable(0 /* selectedIndex */)
+  const activeTabIndex = writable(0)
   const focusedTabPanelIndex = writable(-1)
 
   setContext('lastTabIndex', lastTabIndex)
   setContext('lastTabPanelIndex', lastTabPanelIndex)
   setContext('activeTabIndex', activeTabIndex)
   setContext('focusedTabPanelIndex', focusedTabPanelIndex)
-
-  // $: $activeTabIndex = selectedIndex
-
-  onMount(() => {
-  })
-
 </script>
 
-<div class='tabbed'>
+<div>
   <slot></slot>
 </div>
 
