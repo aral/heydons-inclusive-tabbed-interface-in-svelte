93ebea43e317d68fafd589efd53ed1a96a3ebf9f
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-04-30 09:22:41 +0100

    Implement styles as global due to Svelte limitation
    (and add a note documenting it).

--- src/lib/TabbedInterface/TabList.svelte
+++ src/lib/TabbedInterface/TabList.svelte
@@ -2,33 +2,44 @@
   import { onMount } from 'svelte'
 
   let tabList
+  let role = undefined
 
   onMount(() => {
     // We are doing this here instead of in the DOM
     // as the component is progressively-enhanced.
-    tabList.setAttribute('role', 'tablist')
+    role = 'tablist'
   })
 </script>
 
-<ul bind:this={tabList}>
+<ul {role} bind:this={tabList}>
   <slot></slot>
+  <div id='force'></div>
 </ul>
 
 <style>
-  [role="tablist"] {
+  /*
+    Note: while these styles are included here, they are not scoped. As far as I can see,
+    there isn’t a way to have scoped rules for selectors that do not match DOM elements at
+    compile time (i.e., because they use dynamically-added ARIA roles like we do here).
+
+    I’ve opened an issue on the Svelte repository to track this limitation:
+    https://github.com/sveltejs/svelte/issues/6264
+  */
+
+  :global([role="tablist"]) {
     padding: 0;
   }
 
-  [role="tablist"] li, [role="tablist"] a {
+  :global([role="tablist"] li), :global([role="tablist"] a) {
     display: inline-block;
   }
 
-  [role="tablist"] a {
+  :global([role="tablist"] a) {
     text-decoration: none;
     padding: 0.5rem 1em;
   }
 
-  [role="tablist"] [aria-selected] {
+  :global([role="tablist"] [aria-selected]) {
     border: 2px solid;
     background: #fff;
     border-bottom: 0;
